1f3335badf68c370c76bb99c5e5f9d0829b26a94
TheDonJinee <oma21007@byui.edu>
2023-05-13 18:32:57 +0100

    Swagger update L04

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
         title: 'My API',
         description: 'My Contacts API',
     },
-    host: 'localhost:3000',
-    schemes: ['http'],
+    host: 'contacts-zwae.onrender.com',
+    schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
